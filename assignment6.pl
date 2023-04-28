81afcefc01c6c6aa43ecebd19bcb841c61be9853
Jonah Rinehart <90332834+jrossonrinehart@users.noreply.github.com>
2023-04-28 10:48:40 -0500

    Update assignment6.pl

--- assignment6.pl
+++ assignment6.pl
@@ -1,9 +1,9 @@
-
+%MIN
 findMin([Min], Min).
 findMin([H|T], H) :- findMin(T, MT), H < MT.
 findMin([H|T], MT) :- findMin(T, MT), H >= MT.
 
-
+%Max
 findMax([Max], Max).
 findMax([H|T], H) :- findMax(T, MT), H > MT.
 findMax([H|T], MT) :- findMax(T, MT), H =< MT.
@@ -24,6 +24,8 @@ countElems(L, [H], [Hcnt]) :- countElem(L, H, Hcnt).
 countElems(L, [H|T], [Hcnt|TR]) :- countElem(L, H, Hcnt), countElems(L, T, TR).
 findMode(L, M) :- list_to_set(L, U), countElems(L, U, UC), findMaxU(U, UC, M).
 
+
+%Median
 findmedian(Ls, Med) :- sort(0, @=<, Ls, S), \+isEven(Ls), length(Ls, E), G is floor(E/2), nth0(G, S, Med).
 findmedian(Ls, Med) :- sort(0, @=<, Ls, S), isEven(Ls), length(Ls, E), G is E/2, Z is G-1, nth0(G, S, F), nth0(Z, S, T), findAvg([F, T], Med).
 isEven(Ls) :- length(Ls, N), 0 is mod(N,2).
